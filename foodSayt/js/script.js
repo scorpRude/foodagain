f09b014336e4d24583f8c2281c0a9cba19e677ff
Rasul <scorp_rude_boy@outlook.com>
2024-08-19 21:57:33 +0500

    add timeRemining

--- foodSayt/js/script.js
+++ foodSayt/js/script.js
@@ -1,41 +1,98 @@
 'use strict'
 
-const tabs = document.querySelectorAll('.tabheader__item');
-const tabsContent = document.querySelectorAll('.tabcontent');
-const tabsParent = document.querySelector('.tabheader__items');
+window.addEventListener('DOMContentLoaded', () => {
 
-function hideTabsContent() {
-    tabsContent.forEach(item => {
-        item.classList.remove('show');
-        item.classList.add('hide', 'fade');
-    });
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
 
-    tabs.forEach(item => {
-        item.classList.remove('tabheader__item_active')
-    })
-};
+          
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-function showTabsContent(i = 0) {
-    tabsContent[i].classList.add('show', 'fade');
-    tabsContent[i].classList.remove('hide');
-    tabs[i].classList.add('tabheader__item_active')
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        })
+    }      
 
-};
-
-hideTabsContent();
-showTabsContent();
-
-
-tabsParent.addEventListener('click', (e) => {
-    const target = e.target;
-    
-    if(target && target.classList.contains('tabheader__item')) {
-     tabs.forEach((item, i) => {
-        if (target == item) {
-            hideTabsContent();
-            showTabsContent(i);   
-        };
-     })
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
     }
-});
 
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+          tabs.forEach((item, i) => {
+            if(target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+          })  
+        }
+    })
+
+    //Timer
+
+    const deadline = '2024-09-19';
+
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        const minutes = Math.floor((t / 1000 / 60) % 60);
+        const seconds = Math.floor((t / 1000) % 60);
+
+        return{
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if(num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+    
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector('#days');
+        const hours = timer.querySelector('#hours');
+        const minutes = timer.querySelector('#minutes');
+        const seconds = timer.querySelector('#seconds');
+        const timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+});
